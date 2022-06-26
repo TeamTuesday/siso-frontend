5976dc386b9b73bee55cd8a3d009439ed778babf
kevin_lee <tmdgusdl58@gmail.com>
2022-06-26 14:21:08 +0900

    Fix: [SISO-57] module syntax error 수정

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,18 @@
 import sveltePreprocess from 'svelte-preprocess';
 
 const preprocessOptions = {
-    defaults: {
-        script: "typescript",
-        style: "scss",
-    },
-    scss: {
-        prependData: `@import 'src/variables.scss';`
-    },
+  defaults: {
+    script: 'typescript',
+    style: 'scss'
+  },
+  scss: {
+    prependData: `@import 'src/variables.scss';`
+  }
 };
 
-module.exports = {
-    preprocess: sveltePreprocess(preprocessOptions),
+export default {
+  preprocess: sveltePreprocess(preprocessOptions),
 
-    // Export this to allow rollup.config.js to inherit the same preprocess options.
-    preprocessOptions,
-}
+  // Export this to allow rollup.config.js to inherit the same preprocess options.
+  preprocessOptions
+};
