30379dfc15c0c8c60a9cdb2d3ea40fad4e054445
장유수 <dbtnss@naver.com>
2022-07-06 01:24:47 +0900

    Feat: [SISO-71] 투표 목록 페이지 생성

--- src/Router.svelte
+++ src/Router.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
   import {Route} from 'tinro';
 
+  import Home from '@/pages/Home.svelte';
   import List from '@/pages/List.svelte';
   import Login from '@/pages/Login.svelte';
   import Register from '@/pages/Register.svelte';
 </script>
 
-<Route path="/" redirect="/list" />
+<Route path="/"><Home /></Route>
 <Route path="/list"><List /></Route>
 <Route path="/login"><Login /></Route>
 <Route path="/register"><Register /></Route>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  let votes = [
+    {
+      id: 1,
+      title: '주제 1'
+    },
+    {
+      id: 2,
+      title: '주제 2'
+    },
+    {
+      id: 3,
+      title: '주제 3'
+    },
+    {
+      id: 4,
+      title: '주제 4'
+    },
+    {
+      id: 5,
+      title: '주제 5'
+    }
+  ];
+</script>
+
+<div>
+  <table>
+    <thead>
+      <tr>
+        <th>투표 주제</th>
+        <th>링크</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each votes as vote}
+        <tr>
+          <td>{vote.title}</td>
+          <td><a href="/list">투표하러 가기</a></td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+</div>
+
+<style>
+  div {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  table {
+    width: 300px;
+  }
+  th,
+  td {
+    text-align: center;
+    white-space: nowrap;
+  }
+</style>
