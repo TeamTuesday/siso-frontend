09d2500e60c972f692fa790ba20d344ba4fcc28c
장유수 <peter@gooroomee.com>
2022-07-20 13:36:23 +0900

    Feat: [SISO-71] Vote Page 데이터에 맞게 수정

--- src/pages/List.svelte
+++ src/pages/List.svelte
@@ -1,47 +1,32 @@
 <script lang="ts">
-  import Vote from '@/components/Vote.svelte';
-
   $: votes = fetch('http://13.124.104.234:8080/vote-subjects')
     .then(res => res.json())
     .then((data: {voteSubjects: Module.Ivote[]}) => data.voteSubjects);
-
-  let currentVote = {};
-  const goVote = (vote: Module.Ivote) => {
-    currentVote = vote;
-  };
 </script>
 
 <div class="list-component">
-  {#if currentVote.id}
-    <Vote vote={currentVote} />
-  {:else}
-    <div class="table-wrapper">
-      <table>
-        <thead>
-          <tr>
-            <th>투표 주제</th>
-            <th>링크</th>
-          </tr>
-        </thead>
-        <tbody>
-          {#await votes then votes}
-            {#each votes as vote}
-              <tr>
-                <td>{vote.title}</td>
-                <td>
-                  <button
-                    on:click={() => {
-                      goVote(vote);
-                    }}>투표하러 가기</button
-                  >
-                </td>
-              </tr>
-            {/each}
-          {/await}
-        </tbody>
-      </table>
-    </div>
-  {/if}
+  <div class="table-wrapper">
+    <table>
+      <thead>
+        <tr>
+          <th>투표 주제</th>
+          <th>링크</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#await votes then votes}
+          {#each votes as vote}
+            <tr>
+              <td>{vote.title}</td>
+              <td>
+                <a href="/vote/{vote.id}">투표하러 가기</a>
+              </td>
+            </tr>
+          {/each}
+        {/await}
+      </tbody>
+    </table>
+  </div>
 </div>
 
 <style>
