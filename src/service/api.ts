460253c0eebab86c9e88e98bae6496146cc9b351
kevin_lee <tmdgusdl58@gmail.com>
2022-07-31 17:44:08 +0900

    Feat: [SISO-78] 제너릭설정으로 타입 받아오도록

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const send = async ({method = '', path = '', data = {}} = {}) => {
+const send = async <T>({method = '', path = '', data = {}} = {}) => {
   const commonUrl = 'http://13.124.104.234:8080';
 
   const url = commonUrl + path;
@@ -19,21 +19,22 @@ const send = async ({method = '', path = '', data = {}} = {}) => {
     data
   };
 
-  const response: {data: Record<string, any>} = await axios(options);
+  const response: {data: T} = await axios(options);
+
   return response.data;
 };
 
-const getApi = ({path = ''} = {}) => {
-  return send({method: 'GET', path});
+const getApi = <T>({path = ''} = {}) => {
+  return send<T>({method: 'GET', path});
 };
-const putApi = ({path = '', data = {}} = {}) => {
-  return send({method: 'PUT', path, data});
+const putApi = <T>({path = '', data = {}} = {}) => {
+  return send<T>({method: 'PUT', path, data});
 };
-const postApi = ({path = '', data = {}} = {}) => {
-  return send({method: 'POST', path, data});
+const postApi = <T>({path = '', data = {}} = {}) => {
+  return send<T>({method: 'POST', path, data});
 };
-const delApi = ({path = '', data = {}} = {}) => {
-  return send({method: 'DELETE', path, data});
+const delApi = <T>({path = '', data = {}} = {}) => {
+  return send<T>({method: 'DELETE', path, data});
 };
 
 export {getApi, putApi, postApi, delApi};
