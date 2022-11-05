1a10fb91021522e201199f43b22d392c3ef3a083
장유수 <dbtnss@naver.com>
2022-11-05 16:38:34 +0900

    Chore: [SISO-98] tailwindcss 추가

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: {
+    content: ["./src/**/*.svelte"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      width: {
+        30: "120px"
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+  },
+};
