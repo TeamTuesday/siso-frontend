38c044a363f9b3ec84fa62585008ef74e5f20335
kevin_lee <tmdgusdl58@gmail.com>
2022-11-07 12:48:05 +0900

    임시데이터로 변경

--- src/components/Vote/Vote.svelte
+++ src/components/Vote/Vote.svelte
@@ -3,13 +3,23 @@
   import VoteContents from '@/components/Vote/VoteContents.svelte';
   import VoteInfo from '@/components/Vote/VoteInfo.svelte';
   import Nav from '@/components/Nav.svelte';
-  import {router} from 'tinro';
-  const params = router.params();
 
-  const id = params.id;
-  $: vote = fetch(`http://13.124.104.234:8080/vote-subjects/${id}`)
-    .then(res => res.json())
-    .then((data: {voteSubject: Module.Ivote}) => data.voteSubject);
+  // FIXME : 원래 데이터 가져오던 방식
+
+  // import {router} from 'tinro';
+  // const params = router.params();
+
+  // const id = params.id;
+  // $: vote = fetch(`http://13.124.104.234:8080/vote-subjects/${id}`)
+  //   .then(res => res.json())
+  //   .then((data: {voteSubject: Module.Ivote}) => data.voteSubject);
+
+  // FIXME : 임시데이터로 변경
+  const vote = {
+    title: 'test',
+    agreeDescription: 'agree',
+    oppositeDescription: 'disagree'
+  };
 </script>
 
 <div class="vote-container">
@@ -30,5 +40,6 @@
     display: flex;
     flex-direction: column;
     height: 100%;
+    background: url(/images/sample_background.jpg) no-repeat center / cover;
   }
 </style>
