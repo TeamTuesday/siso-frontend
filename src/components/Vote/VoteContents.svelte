5891f9f3c6bf09cfc4b77cfbf885664d44cf09e7
lsh58 <tmdgusdl58@gmail.com>
2023-02-19 19:47:45 +0900

    애니메이션 구현

--- src/components/Vote/VoteContents.svelte
+++ src/components/Vote/VoteContents.svelte
@@ -2,6 +2,7 @@
   import {voteStore, voteType} from '@/store/voteStore';
   import {voteSubjectStore} from '@/store/voteSubjectStore';
   import Countup from 'svelte-countup';
+  import { bounceOut } from 'svelte/easing';
 
   /** 투표하기*/
   export let vote = async (type: voteType) => {
@@ -14,10 +15,12 @@
           (voteResult.voteAgreeCount / voteResult.voteCount) * 100
         );
         agreeStep = Math.floor(voteResult.voteAgreeCount / 100);
+        agreeGuage = 320 * agreePercent / 100;
         disagreePercent = Math.floor(
           (voteResult.voteDisagreeCount / voteResult.voteCount) * 100
         );
         disagreeStep = Math.floor(voteResult.voteDisagreeCount / 100);
+        disagreeGuage = 320 * disagreePercent / 100;
       });
       votedType = type;
       setTimeout(() => {
@@ -45,10 +48,20 @@
     voteDisagreeCount: 0,
     voteCount: 0
   };
-  export let agreePercent = 0;
-  export let agreeStep = 0;
-  export let disagreePercent = 0;
-  export let disagreeStep = 0;
+  let agreePercent = 0, agreeStep = 0, agreeGuage = 0, agreeGuageRef;
+  let disagreePercent = 0, disagreeStep = 0, disagreeGuage = 0, disagreeGuageRef;
+  function guage(node, { duration, type, width }) {
+    return {
+      duration,
+      css: t => {
+        const eased = bounceOut(t);
+        if(t === 1) {
+          type === 'agree' ? agreeGuageRef.style.width = `${eased * width}px` :  disagreeGuageRef.style.width = `${eased * width}px`
+        }
+        return `width: ${eased * width}px;`
+      }
+    };
+  }
 </script>
 
 <div class="flex flex-col items-center justify-center flex-1 pt-4 pb-2">
@@ -67,13 +80,16 @@
         <Countup
           initial={0}
           value={agreePercent}
-          duration={300}
+          duration={1000}
           step={agreeStep}
           roundto={agreePercent}
           format={false}
         />
+        <span class="vote-percent-suffix" class:change={changed}>%</span>
       </span>
-      <span class="vote-percent-suffix" class:change={changed}>%</span>
+      {#if disagreePercent}
+        <span class="vote-guage agree" in:guage="{{duration: 1500, width: agreeGuage, type: 'agree'}}" bind:this={agreeGuageRef}></span>
+      {/if}
     {:else}
       {agreeDescription}
     {/if}
@@ -88,13 +104,16 @@
         <Countup
           initial={0}
           value={disagreePercent}
-          duration={300}
+          duration={1000}
           step={disagreeStep}
           roundto={disagreePercent}
           format={false}
         />
+        <span class="vote-percent-suffix" class:change={changed}>%</span>
       </span>
-      <span class="vote-percent-suffix" class:change={changed}>%</span>
+      {#if disagreePercent}
+        <span class="vote-guage disagree" in:guage="{{duration: 1500, width: disagreeGuage, type:'disagree'}}" bind:this={disagreeGuageRef}></span>
+      {/if}
     {:else}
       {disagreeDescription}
     {/if}
@@ -110,16 +129,25 @@
     @apply w-full h-[150px] flex justify-center items-center bg-[rgba(255,255,255,0.75)] hover:bg-[rgba(255,255,255,0.85)] shadow-[0_0_20px_4px_rgba(0,0,0,0.06)] rounded-[15px] text-lg text-[#222222] font-bold px-[29px];
   }
   .voted {
-    @apply h-[52px] justify-start  duration-[1500ms] delay-[1500ms];
+    @apply relative h-[52px] justify-start  duration-[1500ms] delay-[1500ms];
+  }
+  .vote-guage {
+    @apply content-[''] absolute left-0 top-0 h-full rounded-[15px] transition-all duration-[1500ms] z-10;
+  }
+  .agree {
+    @apply bg-[#BD8DDB];
+  }
+  .disagree {
+    @apply bg-[#FFD952];
   }
   .vote-percent {
-    @apply text-[60px] leading-[60px] font-medium duration-[1500ms];
+    @apply absolute left-[30px] text-[60px] leading-[60px] font-medium duration-[1500ms] flex items-end z-20;
   }
   .vote-percent.change {
     @apply text-[18px] leading-[28px];
   }
   .vote-percent-suffix {
-    @apply text-[30px] leading-[60px] font-light duration-[1500ms];
+    @apply text-[30px] leading-[30px] font-light duration-[1500ms];
   }
   .vote-percent-suffix.change {
     @apply text-[18px] leading-[28px] font-medium;
