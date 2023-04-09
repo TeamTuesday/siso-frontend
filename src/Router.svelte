15a6fcfdc4cb831909911072b4950e76984853b4
chaameliiia <chaameliiia@gmail.com>
2023-04-09 23:13:28 +0900

    Chore: 페이지별 index.ts 추가

--- src/Router.svelte
+++ src/Router.svelte
@@ -1,18 +1,24 @@
 <script lang="ts">
   import {Route} from 'tinro';
 
-  import Home from '@/pages/Home.svelte';
   import List from '@/pages/List.svelte';
-  import Vote from '@/pages/Vote.svelte';
-  import Login from '@/pages/auth/Login.svelte';
-  import Register from '@/pages/auth/Register.svelte';
-  import MyPage from '@/pages/auth/MyPage.svelte';
+  import * as Auth from '@/pages/auth';
+  import * as Dev from '@/pages/__dev__';
+  import * as Vote from '@/pages/vote';
 </script>
 
-<Route path="/" redirect="/home" />
-<Route path="/home"><Home /></Route>
+<!-- dev only -->
+<Route path="/" redirect="/pagelist" />
+<Route path="/pagelist"><Dev.PageList /></Route>
+
+<!-- main -->
+<!-- <Route path="/" redirect="/list" /> -->
+
+<!-- auth -->
+<Route path="/auth/login"><Auth.LogIn /></Route>
+<Route path="/auth/register"><Auth.Register /></Route>
+<Route path="/auth/mypage"><Auth.MyPage /></Route>
+
+<!-- vote -->
 <Route path="/list"><List /></Route>
-<Route path="/vote/:id"><Vote /></Route>
-<Route path="/auth/login"><Login /></Route>
-<Route path="/auth/register"><Register /></Route>
-<Route path="/auth/mypage"><MyPage /></Route>
+<Route path="/vote/:id"><Vote.Detail /></Route>
